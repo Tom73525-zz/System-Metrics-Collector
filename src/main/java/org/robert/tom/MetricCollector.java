7376981a6f6d6c9d8af1632ae5efad68e72904f5
Robert <muthr@users.noreply.github.com>
2018-04-16 17:47:58 -0400

    Java metrics (#4)
    * Adding Java files for Process and MetricCollector.
    
    * Adding getter/setter methods for State.
    
    * Adding Cpu Usage to Process.
    
    * TO BE TESTED: Implemented metric gathering methods
    
    * Adding implementation for pidList() and collectMetrics(int)
    
    * Improving MetricCollector impl, adding JUnit testing, simple display
    
    * Correcting several warnings

---
+++ src/main/java/org/robert/tom/MetricCollector.java
@@ -0,0 +1,190 @@
+package org.robert.tom;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MetricCollector {
+
+    private ArrayList<Integer> pidList;
+
+    MetricCollector() {
+        pidList = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Integer> getPidList() {
+        return this.pidList;
+    }
+
+    public Double getSystemUptime() {
+        File uptimeFile;
+        Double uptime = null;
+
+        try {
+            uptimeFile = new File("/proc/uptime");
+            if (uptimeFile.exists()) {
+                Scanner scanner = new Scanner(uptimeFile);
+                uptime = scanner.nextDouble();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return uptime;
+    }
+
+    public void gatherCurrentPidList() throws IOException {
+        File procDir;
+        procDir = new File("/proc/");
+
+        if (!procDir.exists()) {
+            throw new FileNotFoundException(procDir.getPath());
+        }
+
+        String[] directories = procDir.list(new FilenameFilter() {
+            public boolean accept(File current, String name) {
+                File tempFile = new File(current, name);
+                return tempFile.getName().matches("[0-9]+") &&
+                        tempFile.isDirectory();
+            }
+        });
+
+        if ((directories != null) && directories.length > 0) {
+            for (String dir : directories) this.pidList.add(Integer.valueOf(dir));
+        }
+    }
+
+    public Process collectMetrics(Integer pid) {
+        Process process = new Process();
+        File statFile = new File("/proc/" + pid.toString() + "/stat");
+        File statusFile = new File("/proc/" + pid.toString() + "/status");
+        File netDevFile = new File("/proc/" + pid.toString() + "/net/dev");
+
+
+        // StatFile HERE
+        process = getStatMetrics(process, statFile);
+
+
+        // STATUSFILE HERE
+        process = getStatusMetrics(process, statusFile);
+
+
+        //NETDEV HERE
+        process = getNetDevMetrics(process, netDevFile);
+
+        return process;
+    }
+
+
+    /**
+     * Retrieving metrics for process with [pid]
+     *   Scans 'stat' file within process directory,
+     *    splitting the file into separate String objects in an array.
+     *   Places values from array into 'process' based on position
+     *    within the line, as described in proc documentation.
+     */
+    public Process getStatMetrics(Process process, File statFile) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(statFile);
+            String[] stats = scanner.nextLine().split("\\s+");
+            if (stats.length == 52) {
+                process.setPid(Integer.decode(stats[0]));
+                process.setName(stats[1].substring(1, stats[1].length() - 1));
+                process.setState(stats[2].charAt(0));
+                process.setPpid(Integer.valueOf(stats[3]));
+                process.setUtime(Long.valueOf(stats[13]));
+                process.setStime(Long.valueOf(stats[14]));
+                process.setCutime(Long.valueOf(stats[15]));
+                process.setCstime(Long.valueOf(stats[16]));
+                process.setNumThreads(Integer.valueOf(stats[19]));
+                process.setStartTime(Long.valueOf(stats[21]));
+            } else {
+                System.out.println("ERROR: Incorrect number of elements in stat file: " + statFile.getPath());
+                return null;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+
+        return process;
+    }
+
+
+    /**
+     * Retrieving VmSize from /proc/[pid]/status
+     *  Scans lines in the file until a line containing the string "VmSize"
+     *    is found.
+     *  Once found, the second to last token in the string (after splitting
+     *    the string using spaces as a delimiter) will be the desired value.
+     */
+    public Process getStatusMetrics(Process process, File statusFile) {
+        Scanner scanner = null;
+        Long vmSize = 0L;
+
+        try {
+            scanner = new Scanner(statusFile);
+            while (scanner.hasNextLine()) {
+                String[] scanLine = scanner.nextLine().split("\\s+");
+                if (scanLine[0].contains("VmSize")) {
+                    vmSize = Long.valueOf(scanLine[scanLine.length - 2]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                process.setVmSize(vmSize);
+                scanner.close();
+            }
+        }
+
+        return process;
+    }
+
+
+    /**
+     * Calculating network traffic to and from process using /proc/[pid]/net/dev
+     *  Skips first two lines of file (they contain header information to lay out
+     *    data in a table).
+     *  For each line after header information, adds 2rd value in line to accumulator
+     *    for bytes received, and adds the 10th value to bytes sent.
+     */
+    public Process getNetDevMetrics(Process process, File netDevFile) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(netDevFile);
+            Long recBytes = 0L;
+            Long sentBytes = 0L;
+
+            if (scanner.hasNextLine()) {
+                scanner.nextLine();
+                scanner.nextLine();
+                while (scanner.hasNextLine()) {
+                    String[] scanLine = scanner.nextLine().trim().split("\\s+");
+                    recBytes += Long.valueOf(scanLine[1]);
+                    sentBytes += Long.valueOf(scanLine[9]);
+                }
+            }
+            process.setBytesReceived(recBytes);
+            process.setBytesSent(sentBytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+
+
+        return process;
+    }
+}
--- src/test/java/org/robert/tom/AppTest.java
+++ src/test/java/org/robert/tom/AppTest.java
@@ -1,38 +1,48 @@
 package org.robert.tom;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void metricRetrievalTest(){
+        Process testProc = new Process();
+        MetricCollector testCollector = new MetricCollector();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        URL statUrl = this.getClass().getResource("/testStat");
+        URL statusUrl = this.getClass().getResource("/testStatus");
+        URL netDevUrl = this.getClass().getResource("/testNetDev");
+
+        System.out.println(statUrl.getFile());
+
+        File testStatFile = new File(statUrl.getFile());
+        File testStatusFile = new File(statusUrl.getFile());
+        File testNetDevFile = new File(netDevUrl.getFile());
+
+        testProc = testCollector.getStatMetrics(testProc, testStatFile);
+        testProc = testCollector.getStatusMetrics(testProc, testStatusFile);
+        testProc = testCollector.getNetDevMetrics(testProc, testNetDevFile);
+
+        assertEquals(371, testProc.getPid());
+        assertEquals("ips-monitor", testProc.getName());
+        assertEquals('S', testProc.getState());
+        assertEquals(2, testProc.getPpid());
+        assertEquals(30, testProc.getUtime());
+        assertEquals(67, testProc.getStime());
+        assertEquals(352, testProc.getCutime());
+        assertEquals(985, testProc.getCstime());
+        assertEquals(1, testProc.getNumThreads());
+        assertEquals(1289, testProc.getStartTime());
+        assertEquals(0, testProc.getVmSize());
+        assertEquals(3145786, testProc.getBytesSent());
+        assertEquals(22216168, testProc.getBytesReceived());
     }
 }
