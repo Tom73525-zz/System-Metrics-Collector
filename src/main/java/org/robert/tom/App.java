b5faf1aca838ca9a0ba7c437fd5d58356971650c
Robert <muthr@users.noreply.github.com>
2018-04-19 17:01:03 -0400

    Added additional functionality to MetricCollector (#7)
    Namely, added function with signature:
    	public ArrayList<Process> collectMetrics();

--- src/main/java/org/robert/tom/App.java
+++ src/main/java/org/robert/tom/App.java
@@ -2,6 +2,10 @@ package org.robert.tom;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -17,14 +21,14 @@ public class App {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Setting up Objects
-        ArrayList<Process> procs = new ArrayList<Process>();
-        MetricCollector mc = new MetricCollector();
+        final ArrayList<Process> procs = new ArrayList<Process>();
+        final MetricCollector mc = new MetricCollector();
 
         // Gathering metrics
         try {
             mc.gatherCurrentPidList();
             for (Integer currentPid : mc.getPidList()) {
-                procs.add(mc.collectMetrics(currentPid));
+                procs.add(mc.collectMetricsForPid(currentPid));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,8 +36,8 @@ public class App {
 
 
         // TABLE SETUP
-        JTable table = new JTable();
-        DefaultTableModel tableModel = new DefaultTableModel(0, 0);
+        final JTable table = new JTable();
+        final DefaultTableModel tableModel = new DefaultTableModel(0, 0);
         tableModel.setColumnIdentifiers(columns);
         table.setModel(tableModel);
 
@@ -43,11 +47,57 @@ public class App {
             tableModel.addRow(metricArray);
         }
 
+
+        // BUTTON SETUP
+        JButton updateButton = new JButton("Refresh");
+        updateButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                procs.clear();
+
+                try {
+                    mc.gatherCurrentPidList();
+                    for (Integer currentPid : mc.getPidList()) {
+                        procs.add(mc.collectMetricsForPid(currentPid));
+                    }
+
+                    tableModel.setRowCount(0);
+
+                    for (Process proc : procs) {
+                        Object[] updateMetricArray = proc.getProcessArray();
+                        tableModel.addRow(updateMetricArray);
+                    }
+
+                    tableModel.fireTableDataChanged();
+
+                } catch (IOException updateExeption) {
+                    updateExeption.printStackTrace();
+                }
+            }
+        });
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        JPanel metricsContainer = new JPanel(new GridBagLayout());
+        frame.getContentPane().add(metricsContainer);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.gridheight = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        metricsContainer.add(updateButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 8;
+        gbc.weighty = 8;
+        gbc.gridheight = 8;
+        gbc.fill = GridBagConstraints.BOTH;
         JScrollPane scrollPane = new JScrollPane(table);
-        frame.getContentPane().add(scrollPane);
+        metricsContainer.add(scrollPane, gbc);
 
         frame.pack();
-        frame.setSize(500, 300);
+        frame.setSize(960, 720);
         frame.setVisible(true);
     }
 
