5753dc53a12c364b30579470c7742d2c65b05202
tom <tomjben7@gmail.com>
2018-04-22 19:22:19 -0400

    Added getCPUUsage() with minor changes to the saveProcessMetrics(). Added documentation to all existing methods.

--- src/test/java/org/robert/tom/DB_Population_Test.java
+++ src/test/java/org/robert/tom/DB_Population_Test.java
@@ -1,16 +1,37 @@
 package org.robert.tom;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class DB_Population_Test {
 
+  /**
+   * This method tests for the population of the database
+   */
   @Test
   public void testDB(){
     SQLAdapter sqlAdapter = new SQLAdapter();
-    boolean isMetricsSaved =   sqlAdapter.saveProcessMetrics(112,50,"firefox",
-            1024,'S',2,53,643,1324,323,
-            45,12.3);
+    ArrayList<MCAProcess> mcaProcesses = new ArrayList<MCAProcess>();
+    MCAProcess mcaProcess = new MCAProcess();
+    mcaProcess.setCpuUsage(12.3);
+    mcaProcess.setStartTime(0L);
+    mcaProcess.setUtime(43L);
+    mcaProcess.setStime(22L);
+    mcaProcess.setCutime(2L);
+    mcaProcess.setCstime(5L);
+    mcaProcess.setNumThreads(2);
+    mcaProcess.setState('S');
+    mcaProcess.setVmSize(432L);
+    mcaProcess.setName("firefox");
+    mcaProcess.setPpid(44);
+    mcaProcess.setPid(10);
+    mcaProcesses.add(mcaProcess);
+    sqlAdapter.saveProcessMetrics(mcaProcesses);
+    sqlAdapter = new SQLAdapter();
+    boolean isMetricsSaved =   sqlAdapter.saveProcessMetrics(mcaProcesses);
 
     assertEquals(isMetricsSaved ,true);
   }
